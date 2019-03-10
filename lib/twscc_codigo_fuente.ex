8ca1e926ae7a103b9eecadba324948edf01627d8
Erik Valdez Mondragon <perik_92@hotmail.com>
2019-03-10 02:53:10 -0600

    pequeña modificacion a un mensaje en el codigo fuente

--- lib/twscc_codigo_fuente.ex
+++ lib/twscc_codigo_fuente.ex
@@ -2,16 +2,17 @@ defmodule Proyecto_compilador do
   @moduledoc """
   Documentacion for proyecto_compilador.
   """
-  
+
   ##para compilar mix escript.build
-  #ejecuta ./proyecto_compilador
+  #ejecuta ./twscc_compilador_c
+
 
   def main(_args) do ##no te quejes compilador
-  
+
     ##marzo 5, 2019
-    
-    IO.puts("Holi! a compilar!");
-    file_content=  File.read!("/home/eriq/Documentos/Procesos_UNIX/ejercicio1.c"); ##lectura del archivo
+
+    IO.puts("Escaner del compilador. Lectura del codigo fuente");
+    file_content=  File.read!("/home/eriq/compiladores/twscc/codigo_prueba_return.c"); ##lectura del archivo
     IO.inspect(file_content);
     ##dar por hecho que el codigo viene mal
     ##hacer un modulo de saneamiento de los datos
@@ -26,85 +27,85 @@ defmodule Proyecto_compilador do
     ##definicion de una expresion regular ~r/"expresion regular"/
     words = Regex.split(~r/\s+/, trimmed_content);
     IO.inspect(words);
-    
+
     tokens_separados =  scan_word(words);
     IO.inspect(tokens_separados);
-    
-    #DICHA LISTA DE SALIDA YA SALDRÁ EN ORDEN PARA CONSTRUIR 
-    
+
+    #DICHA LISTA DE SALIDA YA SALDRÁ EN ORDEN PARA CONSTRUIR
+
     ##ver todos los casos posibles para dividir, probando por ejemplo todo el codigo en el mismo renglon
     #"main(){return" buscar los tokens a identifiar (palabras reservadas: main, (, ), {, }, return, etc..)
     ##utilizar atomos para etiquetas (:main, :return) por ejemplo, la tupla del 2 {:integer, 2}
-    ## para main: {:keyword o id, "main"} 
- 
+    ## para main: {:keyword o id, "main"}
+
   end
-  
-  
+
+
   def scan_word(words) do
     Enum.flat_map(words, &lex_raw_tokens/1); ##(coleccion [lista], funcion) #va a iterar contra los elementos de la lista. por cada elemento iterado se lo pasa a la funcion
-    
+
     #el /1 indica la cantidad de parámetros que recibe la funcion
     end
-  
-  
+
+
   #identificar que es un numero (uno o varios)
   def get_constant(program) do
-    case Regex.run(~r/\d+/, program) do #recibe expresion regular con digito: Regex.run(~r/\d+/, "123213jjasdasdasd")["123213"]    
+    case Regex.run(~r/\d+/, program) do #recibe expresion regular con digito: Regex.run(~r/\d+/, "123213jjasdasdasd")["123213"]
       #pattern matching para extraer el valor de la lista que devuelve run
       #devuelve tupla con etiqueta {:constante, 1234}                       ##quita el valor numerico de cadena y devuelve el remanente
       [valor] -> {{:constant, String.to_integer(valor)}, String.trim_leading(program, valor)}
       end
   end
-  
-  
+
+
   ##7 marzo 19
     #Crear funcion que reciba la cadena y partirla reconociendo los tokens y devolverá una lista de tokens [{a, b}, {c, d}]
-    
-    ##implementar algoritmo, tomando en cuenta las tuplas simples o dobles, 
+
+    ##implementar algoritmo, tomando en cuenta las tuplas simples o dobles,
   ##crear funcion que recibe un "programa" (la cadena words)
-  
+
   #ejecutar con "iex -S mix" para poder ejecutar la funcion
-  
+
   #HACERLO RECURSIVO y romperlo mientras program no esté vacía
   def lex_raw_tokens(program) when program != "" do #compara la cadena para ver que caso cumple
-  
-     
+
+
     #lo que devuela el case, haz pattern matching contra esto para recuperarlo...
     {token, cadena_restante} =
     case program do #case de la cadena
-    
+
                             ##-> si "program" hace match contra el caso, ejecuta... {} (devuelve la tupla)
         "{" <> cadena_restante -> {:open_brace, cadena_restante} #hará pattern maching contra "{" revisando que la cadena empiece con llave y todo lo demás lo guarda en "rest". Esto ya genera el tokens (devuelve la tupla de dos elementos)
-        
+
         "}" <> cadena_restante -> {:close_brace, cadena_restante}
-        
+
         "(" <> cadena_restante -> {:open_paren, cadena_restante}
-        
+
         ")" <> cadena_restante -> {:close_paren, cadena_restante}
-        
+
         ";" <> cadena_restante -> {:semicolon, cadena_restante}
-        
+
         "return" <> cadena_restante -> {:return_Keyword, cadena_restante}
-        
+
         "int" <> cadena_restante -> {:int_Keyword, cadena_restante}
-        
+
         "main" <> cadena_restante -> {:main_keyword, cadena_restante}
-        
+
         #el DEFAULT del case
         cadena_restante -> get_constant(cadena_restante) #crea funcion y pasale la cadena que no cumplió con los casos anteriores
         end
-        
+
     #devuelve recursirvamente la cadena_restante para seguir revisando
     #tokens_restantes es la cadena restante con los caracteres sin separar hasta que se vacíe
     tokens_restantes = lex_raw_tokens(cadena_restante) #añadir este token al final de la lista
     [token | tokens_restantes] ##agrega los tokens restantes AL FINAL DE la lista de token
-    
+
   end
-  
+
   def lex_raw_tokens(_program) do #debe haber otra funcion llamada igual para recibir la cadena vacia (en realidad cualquiera)
     [] ##para devolver una lista vacía
   end
-  
+
   #salida final del "exer" iex(3)> Proyecto_compilador.lex_raw_tokens("main(){}")
   #[:main_keyword, :open_paren, :close_paren, :open_brace, :close_brace]
 
