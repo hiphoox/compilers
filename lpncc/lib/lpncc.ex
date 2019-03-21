74af49f5bcf1883db07e808525b2024fcdd31424
Wolf-08 <alejandro.montecillo@gmail.com>
2019-03-21 10:06:45 -0600

    Inicio del Proyecto

---
+++ lpncc/lib/lpncc.ex
@@ -0,0 +1,14 @@
+defmodule Lpncc do
+  @moduledoc """
+  Documentation for Lpncc.
+  """
+
+   def main(_args) do
+    file_content=File.read!("examples/return2.c") 
+    trimmed_content=String.trim(file_content)
+    words = Regex.split(~r/\s+/,trimmed_content)
+    tokens=Lexer.scan_words(words)
+    #IO.puts(words)
+    IO.inspect(tokens) #Loguardaenunalista
+  end
+end
---
+++ lpncc/test/lpncc_test.exs
@@ -0,0 +1,8 @@
+defmodule LpnccTest do
+  use ExUnit.Case
+  doctest Lpncc
+
+  test "greets the world" do
+    assert Lpncc.hello() == :world
+  end
+end
