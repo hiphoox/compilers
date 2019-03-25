cd716cae4ccb0ce48b3429ede6b2fe4abae7a774
Alan Camargo <camargo.alan.m@gmail.com>
2019-03-24 19:51:21 -0600

    inicio del proyecto

---
+++ compiler/lib/compiler.ex
@@ -0,0 +1,18 @@
+defmodule Compiler do
+  @moduledoc """
+  Documentation for Compiler.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Compiler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
