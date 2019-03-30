00bc672df33e9fc5d4d379916a8ec532e1bf399c
CharlieML <charls_brian@hotmail.es>
2019-03-30 03:06:54 -0600

    Se han creado los archivos ProyectoCompilador.ex y Lexer.ex

---
+++ ProjectCompiler-twscc/Lexer.ex
@@ -0,0 +1,3 @@
+defmodule Lexer do
+
+end
---
+++ ProjectCompiler-twscc/ProyectoCompilador.ex
@@ -0,0 +1,13 @@
+defmodule Proyecto_compilador do
+  
+  def main(argv) do
+
+    case length(argv) do
+      0 ->
+      1 ->
+      2 ->
+    end
+
+  end
+
+end
