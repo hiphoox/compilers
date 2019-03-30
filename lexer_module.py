29d582e3f372a24bb7fd460d366e7f6bd8008fa7
Tenkoni <caliminion@gmail.com>
2019-03-30 14:51:34 -0600

    Se mejoró la detección de tókens inválidos y números enteros.

--- lexer_module.py
+++ lexer_module.py
@@ -2,13 +2,8 @@ import re
 
 def lexer(plaintext):
 	keywords = re.compile('[a-zA-Z]+|[\{,\},\(,\),\;]|[0-9]+')
-	
-	unknown_inputs = re.sub(keywords, '', plaintext)
-	if unknown_inputs.strip(): #to detect the existence unknown tokens
-		print("Invalid input detected at file. List of unknown tokens: ")
-		print(unknown_inputs.split())
-		raise SystemExit() 
 
+	unknown_inputs = re.sub(keywords, '', plaintext)
 	raw_tokens = re.findall(keywords, plaintext)
 	token_list = []
 	for token in raw_tokens:
@@ -28,7 +23,14 @@ def lexer(plaintext):
 			token_list.append('bracket_close')
 		elif token == ';':
 			token_list.append('semicolon')
-		else:
+		elif (re.match(r'[0-9]+', token) is not None): #to detect integers
 			token_list.append(tuple(['integer',int(token)]))
-	
+		else: #the unknown tokens would enter here
+			unknown_inputs+= " " + token
+
+	if unknown_inputs.strip(): #to displaye the error in case of a failed lexical analysis
+		print("Lexical error, invalid input detected at file. List of unknown tokens: ")
+		print(unknown_inputs.split())
+		raise SystemExit()
+
 	return (token_list)
