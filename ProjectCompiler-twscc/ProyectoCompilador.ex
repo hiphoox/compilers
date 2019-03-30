b3fcf9e96569f1d1c0eaf880e7865e2e1f296887
CharlieML <charls_brian@hotmail.es>
2019-03-30 03:56:39 -0600

    Modulo Proyecto_compilador: Funcion main(argv): Se añade acceso e información para la funcion help() en la condicion case

--- ProjectCompiler-twscc/ProyectoCompilador.ex
+++ ProjectCompiler-twscc/ProyectoCompilador.ex
@@ -3,13 +3,18 @@ defmodule Proyecto_compilador do
   def main(argv) do
 
     case length(argv) do
-      0 ->
-      1 ->
-      2 ->
+      0 -> IO.puts("Compilador de C de twscc. Escriba --help para la ayuda.");
+      1 -> is_help = Enum.at(argv, 0) =~ "--help";
+           if is_help do
+             help();
+           else
+             IO.puts("ERROR, comando inválido");
+           end
+      2 -> 
     end
 
   end
-  
+
   def help() do
     IO.puts("Usage:\n ./twscc_compilador_c [option] nombre del archivo.c")
     IO.puts("Options:\n")
