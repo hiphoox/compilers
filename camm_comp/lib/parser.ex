4dfbf2e78f4166c8280ad02d18274df984f052ab
Alejandro Hernández Albino <alejanndro.hdz@gmail.com>
2019-03-30 19:56:16 -0600

    Se agregó un AST báscio al modulo parser, y se añadieron comentarios al modulo main

--- camm_comp/lib/parser.ex
+++ camm_comp/lib/parser.ex
@@ -1,3 +1,23 @@
 defmodule Parser do
+	@type exp ::  integer
+
+  @type keywords :: {:intkeyword, charlist}
+									|{:returnkeyword, charlist}
+									|{:mainkeyword, charlist}
+
+	@type caracter :: {:open_brance, char}
+									| {:close_brance, char}
+									| {:open_parenthesis, char}
+									| {:close_parenthesis, char}
+									| {:semicolon, char}
+
+
+  @type statement :: keywords , exp , caracter
+
+  @type functions :: keywords, keywords, caracter,
+										caracter,caracter ,statement, caracter
+
+  @type program :: function
+
 	
 end
