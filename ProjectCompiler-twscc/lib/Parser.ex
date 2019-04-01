597cfd217cddb0bd577393e3cb0e90a6b56db456
CharlieML <charls_brian@hotmail.es>
2019-04-01 11:31:06 -0600

    Modulo Parser: Se ha definido la funcion parse_program(token_list)

--- ProjectCompiler-twscc/lib/Parser.ex
+++ ProjectCompiler-twscc/lib/Parser.ex
@@ -14,5 +14,14 @@ defmodule Parser do
     #devuelve la misma lista sin alterar.
     {nil, lista}
   end
-end
+  end
+
+  def parse_program(token_list) do
+    IO.puts("*NODO PROGRAMA, GENERANDO RAMAS:")
+    #llamando a parse_function(list)
+    {nil, token_list}= parse_function(token_list);
+    IO.puts("*NODO PROGRAMA FINALIZADO")
+    IO.inspect(token_list, label: "Lista de tokens restantes");
+    nil; #en esta linea debería regresar un árbol
+  end
 end
