1d3032ca6a1a2dca597e6f599d0d98f6981b9016
CharlieML <charls_brian@hotmail.es>
2019-04-01 11:18:23 -0600

    Modulo Parser: Se ha definido la funcion parsero(token_list)

--- ProjectCompiler-twscc/lib/Parser.ex
+++ ProjectCompiler-twscc/lib/Parser.ex
@@ -1,3 +1,6 @@
 defmodule Parser do
-  
+  def parsero(token_list) do
+   #llamada a creación de la gramática para el AST, debería devolver un árbol. Actualmente regresa "nil"
+   ast_tree = parse_program(token_list);
+ end
 end
