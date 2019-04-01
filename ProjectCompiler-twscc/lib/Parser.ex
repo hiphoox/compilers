37e316f3517f91ff62110fc00194b6e2a7e83948
CharlieML <charls_brian@hotmail.es>
2019-04-01 11:25:05 -0600

    Modulo Parser: Se ha definido la funcion parsear(lista,atom)

--- ProjectCompiler-twscc/lib/Parser.ex
+++ ProjectCompiler-twscc/lib/Parser.ex
@@ -3,4 +3,16 @@ defmodule Parser do
    #llamada a creación de la gramática para el AST, debería devolver un árbol. Actualmente regresa "nil"
    ast_tree = parse_program(token_list);
  end
+
+ def parsear(lista, atom) do
+  #extrae primer elemento de la lista y lo compara con átomo o si es tupla {constante, 4}
+  if (List.first(lista) == atom) do
+    #devuelve el primer elemento de la lista y bórralo
+    {List.first(lista), List.delete(lista, atom)};
+  else
+    IO.puts("Error en el parseo");
+    #devuelve la misma lista sin alterar.
+    {nil, lista}
+  end
+end
 end
