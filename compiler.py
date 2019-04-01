0e690d9725ef07ae1f445e437b307a6774388942
Tenkoni <caliminion@gmail.com>
2019-04-01 16:35:37 -0600

    Se añadió el manejo de los argumentos -o, -s, -t y -a

--- compiler.py
+++ compiler.py
@@ -1,15 +1,22 @@
 import  sys
+import argparse
 from file_clean import *
 from lexer_module import *
 
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+parser.add_argument("file", type=str, help="name of the C file")
+parser.add_argument("-o", "--output", type=str, help="Write output to <OUTPUT>")
+group.add_argument("-s" , "--ensamblador", action = "store_true", help="Generates the assembler file")
+group.add_argument("-t", "--tokens", action = "store_true", help="Generates the token list and display it on the terminal")
+group.add_argument("-a", "--ast", action = "store_true", help="Generates the AST and display it on the terminal")
+
+args = parser.parse_args()
+
 try:
-	raw_text = read_file(sys.argv[1])
+	raw_text = read_file(args.file)
 except IOError as e: #excepción en caso de que el archivo no exista 
-	print(e)
-	print("Unable to read the file ", sys.argv[1])
-except IndexError as e: #excepción en caso de que 
-	print(e)
-	print("No arguments were provided")
+	raise SystemExit(e)
 
 token_list = lexer(raw_text)
 print(token_list)
