a6eaabce6e21cf66724f21d4a7910013af498301
CharlieML <charls_brian@hotmail.es>
2019-04-01 20:35:18 -0600

    Modulo Parser: parse_exp(token_list): Se redefine pattern matching {nil,token_list,node} de condicion case

--- ProjectCompiler-twscc/lib/Parser.ex
+++ ProjectCompiler-twscc/lib/Parser.ex
@@ -27,28 +27,28 @@ defmodule Parser do
 
    def parse_function(token_list) do
      IO.puts("*NODO FUNCION, GENERANDO RAMAS:")
-#parsear int main(){...
-#{elemento extraido, lista tokens} a almacenar en la variable. Util para el AST}
-{a, token_list} = parsear(token_list, :int_Keyword);
-{b, token_list} = parsear(token_list, :main_Keyword);
-{c, token_list} = parsear(token_list, :open_paren);
-{d, token_list} = parsear(token_list, :close_paren);
-{e, token_list} = parsear(token_list, :open_brace);
-    IO.inspect(token_list, label: "Lista de tokens");
-    ##mostrando elementos extraidos
-IO.inspect(a, label: "T");
-IO.inspect(b, label: "NT");
-IO.inspect(c, label: "T");
-IO.inspect(d, label: "T");
-IO.inspect(e, label: "T");
-##antes de construir el nodo de la funcion main, parse las declaraciones a continuacion
-{nil, token_list} = parse_statement(token_list);
-#si el parseo anterior fue correcto, finaliza el nodo funcion y créalo hacia arriba
-{g, token_list} = parsear(token_list, :close_brace);
-IO.inspect(g, label: "T");
-IO.puts("*NODO FUNCION FINALIZADO")
- func={:function,[a,b,c,d,e,state,g]}##aqui se agregan los nodos
-{nil, token_list};  ##se vuelve a poner, es lo que devolverá esta funcion
+    #parsear int main(){...
+    #{elemento extraido, lista tokens} a almacenar en la variable. Util para el AST}
+    {a, token_list} = parsear(token_list, :int_Keyword);
+    {b, token_list} = parsear(token_list, :main_Keyword);
+    {c, token_list} = parsear(token_list, :open_paren);
+    {d, token_list} = parsear(token_list, :close_paren);
+    {e, token_list} = parsear(token_list, :open_brace);
+        IO.inspect(token_list, label: "Lista de tokens");
+        ##mostrando elementos extraidos
+    IO.inspect(a, label: "T");
+    IO.inspect(b, label: "NT");
+    IO.inspect(c, label: "T");
+    IO.inspect(d, label: "T");
+    IO.inspect(e, label: "T");
+    ##antes de construir el nodo de la funcion main, parse las declaraciones a continuacion
+    {nil, token_list, state} = parse_statement(token_list);
+    #si el parseo anterior fue correcto, finaliza el nodo funcion y créalo hacia arriba
+    {g, token_list} = parsear(token_list, :close_brace);
+    IO.inspect(g, label: "T");
+    IO.puts("*NODO FUNCION FINALIZADO")
+    func={:function, b, [state]}##aqui se agregan los nodos
+    {nil, token_list, func};  ##se vuelve a poner, es lo que devolverá esta funcion
    end
 
    def parse_statement(token_list) do
@@ -56,7 +56,7 @@ IO.puts("*NODO FUNCION FINALIZADO")
      ##parsear {return 2;...
      {h, token_list} = parsear(token_list, :return_Keyword);
      #deriva una expresión
-     {nil, token_list,exp} = parse_exp(token_list);
+     {nil, token_list, exp} = parse_exp(token_list);
      {j, token_list} = parsear(token_list, :semicolon);
 
      IO.inspect(h, label: "T");
@@ -65,23 +65,26 @@ IO.puts("*NODO FUNCION FINALIZADO")
      IO.puts("*NODO STATEMENT FINALIZADO")
      #devuelve nil, token para que function continue utilizando la lista
 
-     state={:statement,[h,exp,j]}#definicion de la expresion statement
+     state={h, nil , [exp] }#definicion de la expresion statement
 
      {nil, token_list,state};
+
    end
 
    def parse_exp(token_list) do
-      IO.puts("NODO EXPRESIÓN, GENERANDO RAMAS")
+     IO.puts("NODO EXPRESIÓN, GENERANDO RAMAS")
 
-      {i, token_list} =
-      case List.first(token_list) do
-        #aquí puede haber dos derivaciones de la constante, 2 o 4. Util para cuando tengamos que ampliar la gramática
-        {:constant, 2} -> parsear(token_list, {:constant, 2});
-        {:constant, 4} -> parsear(token_list, {:constant, 4});
-      end
-      IO.inspect(i, label: "T")
-      IO.puts("NODO EXPRESIÓN FINALIZADO")
-      exp={:exp,[elem(i,1)]}
-      {nil, token_list,exp}
+     {nil, token_list, node} =
+     case List.first(token_list) do
+       #aquí puede haber dos derivaciones de la constante, 2 o 4. Util para cuando tengamos que ampliar la gramática
+       {:constant, 2} -> parse_constant(token_list);
+
+
+     end
+
+
+     IO.puts("NODO EXPRESIÓN FINALIZADO")
+     exp={elem(node, 0), elem(node, 1), [node]}
+     {nil, token_list, node}
     end
 end
