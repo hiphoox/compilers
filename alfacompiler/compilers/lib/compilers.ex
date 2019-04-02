3b0cf3d0d22a9673b790b9ad637bdb72eb1f04ad
PaolaElizabethSanchez <paolaescastillo@gmail.com>
2019-04-01 23:10:48 -0600

    Se añade complemento en el main para la verificación del lexer, unión exitosa

--- alfacompiler/compilers/lib/compilers.ex
+++ alfacompiler/compilers/lib/compilers.ex
@@ -7,6 +7,8 @@ defmodule compilers do
     file_content = File.read!("examples/return_2.c")
     trimmed_content = String.trim(file_content)
     words = Regex.split(~r/\s+/, trimmed_content)
-    
+    tokens = Lexer.scan_words(words)
+    IO.inspect(tokens)
+
   end
 end
