441c86b15c78d4f7f46f125e9383d451bfee7588
CharlieML <charls_brian@hotmail.es>
2019-04-02 02:17:59 -0600

    Modulo Generador_codigo: Se ha definido la funcion privada postorder({},code)

--- ProjectCompiler-twscc/lib/Generador_codigo.ex
+++ ProjectCompiler-twscc/lib/Generador_codigo.ex
@@ -2,9 +2,15 @@ defmodule Generador_codigo do
 
   def assembly(ast) do
 
-    IO.inspect(ast);
+        #f = fn x -> IO.write "#{x} " end
+        IO.write "\npostorder:  "
 
+        code = postorder(ast, "")
+        IO.puts("\n")
+        IO.puts(code)
 
   end ##termina funcion post_order_arbol
-
+  #sin hijos el nodo
+  defp postorder({}, code), do: code;
+  #nodo con hijos
 end
