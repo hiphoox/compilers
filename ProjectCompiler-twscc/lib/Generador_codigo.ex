a0c84668e4473588bfa2f0ef9841c10189e215d7
CharlieML <charls_brian@hotmail.es>
2019-04-04 07:35:17 -0600

    Se creo el archivo Linker.ex/Modulo Linker

--- ProjectCompiler-twscc/lib/Generador_codigo.ex
+++ ProjectCompiler-twscc/lib/Generador_codigo.ex
@@ -2,13 +2,13 @@ defmodule Generador_codigo do
 
   def assembly(ast) do
 
-        #IO.write "\nPostorden del AST\n"
-        #llama a la funcion que recorre en post orden y devuelve el código
-        code = postorden(ast, "")
+    #IO.write "\nPostorden del AST\n"
+    #llama a la funcion que recorre en post orden y devuelve el código
+    code = postorden(ast, "")
 
   end
   #sin hijos el nodo
-  defp postorder({}, code), do: code;
+  defp postorden({}, code), do: code;
   #nodo con hijos
   defp postorden({atomo, value, izquierda ,derecha }, code) do
     #recursividad, revisa {:node, data, {izquierda}, {}}
@@ -18,26 +18,26 @@ defmodule Generador_codigo do
     #si ya no encuentra más hijos, extrae el valor, genera el código y concatenalo con "code"
     #IO.inspect(value, label: "Dato obtenido")
     codigo_gen(atomo, value, code);
+
+  end
+#funciones "sobreescritas"
+  def codigo_gen(:program, _, codigo) do
+    """
+    .section        __TEXT,__text,regular,pure_instructions
+    .p2align        4, 0x90
+    """ <> codigo #concatena esto antes del codigo
   end
 
-  #funciones "sobreescritas"
-    def codigo_gen(:program, _, codigo) do
-      """
-      .section        __TEXT,__text,regular,pure_instructions
-      .p2align        4, 0x90
-      """ <> codigo #concatena esto antes del codigo
-    end
+  def codigo_gen(:function, _, codigo) do
+    """
+      .globl  _main         ## -- Begin function main
+  _main:                    ## @main
+  """ <> codigo #concatena esto antes del codigo
+  end
 
-    def codigo_gen(:function, _, codigo) do
-      """
-        .globl  _main         ## -- Begin function main
-    _main:                    ## @main
-    """ <> codigo #concatena esto antes del codigo
-    end
-
-    def codigo_gen(:constant, value, _) do
-      "$#{value}" #guarda el valor de la constate
-    end
+  def codigo_gen(:constant, value, _) do
+    "$#{value}" #guarda el valor de la constate
+  end
   ##pega el valor de la constante y añade una instruccion return
   def codigo_gen(:return_Keyword, _, codigo) do
     """
---
+++ ProjectCompiler-twscc/lib/Linker.ex
@@ -0,0 +1,3 @@
+defmodule Linker do
+
+end
