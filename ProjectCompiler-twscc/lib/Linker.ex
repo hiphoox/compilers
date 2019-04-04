d66090d8dd5a0dedcedbce3a7068f88c238ef3c0
CharlieML <charls_brian@hotmail.es>
2019-04-04 09:07:23 -0600

    Modulo Linker: genera_archivo_binario(ruta_ensamblador): Se ha cambiado el nombre ruta_ensamblador por nombre_archivo_ensamblador

--- ProjectCompiler-twscc/lib/Linker.ex
+++ ProjectCompiler-twscc/lib/Linker.ex
@@ -1,6 +1,6 @@
 defmodule Linker do
   def genera_archivo_binario(ruta_ensamblador) do
-    ruta_ensamblador = Path.basename(ruta_ensamblador)##Returns the last component of the path or the path itself if it does not contain any directory separators
+  nombre_archivo_ensamblador= Path.basename(ruta_ensamblador)##Returns the last component of the path or the path itself if it does not contain any directory separators
     nombre_archivo_binario = Path.basename(ruta_ensamblador, ".s")#Returns the last component of path with the extension stripped
     output_dir_name = Path.dirname(ruta_ensamblador)#Returns the directory component of path
     ruta_ensamblador = output_dir_name <> "/" <> ruta_ensamblador
