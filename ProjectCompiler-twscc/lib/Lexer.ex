1c7a63a065f07cca1a2df01d9b0aeee5718d38cc
CharlieML <charls_brian@hotmail.es>
2019-04-04 06:32:54 -0600

    Modulo Saneador: Se quitan comentarios y se agrega valor de retorno nil en condifion if else

--- ProjectCompiler-twscc/lib/Lexer.ex
+++ ProjectCompiler-twscc/lib/Lexer.ex
@@ -1,24 +1,24 @@
 defmodule Lexer do #separando el lexer del modulo principal, mueve funcion (refactoring)
 
 
-  def scan_word(file_content) do
-    #IO.inspect(file_content)
-    ##llamar al saneador, si no hay error devolverá las palabras sin saltos de linea en su totalidad
-    words = Saneador.fix_code_format(file_content);
+def scan_word(file_content) do
+  #IO.inspect(file_content)
+  ##llamar al saneador, si no hay error devolverá las palabras sin saltos de linea en su totalidad
+  words = Saneador.fix_code_format(file_content);
 
-    try do
-      #IO.puts("\nGenerando lista de tokens (LEXER)")
-      Enum.flat_map(words, &lex_raw_tokens/1); ##(coleccion [lista], funcion) #va a iterar contra los elementos de la lista. por cada elemento iterado se lo pasa a la funcion
-    rescue
-      Protocol.UndefinedError -> nil
-      ArgumentError -> IO.puts("Error: La sintaxis del código es inválida.")
-    end
-    #el /1 indica la cantidad de parámetros que recibe la funcion
-    end
+  try do
+    #IO.puts("\nGenerando lista de tokens (LEXER)")
+    Enum.flat_map(words, &lex_raw_tokens/1); ##(coleccion [lista], funcion) #va a iterar contra los elementos de la lista. por cada elemento iterado se lo pasa a la funcion
+  rescue
+    Protocol.UndefinedError -> nil
+    ArgumentError -> IO.puts("Error: La sintaxis del código es inválida.")
+  end
+  #el /1 indica la cantidad de parámetros que recibe la funcion
+  end
 
 
-  #identificar que es un numero (uno o varios)
-  def get_constant(program) do
+#identificar que es un numero (uno o varios)
+def get_constant(program) do
     #puede leer enteros negativos, sin embargo el parser no los dejará pasar
       try do
       case Regex.run(~r/-?\d+/, program) do #recibe expresion regular con digito: Regex.run(~r/\d+/, "123213jjasdasdasd")["123213"]
@@ -29,23 +29,23 @@ defmodule Lexer do #separando el lexer del modulo principal, mueve funcion (refa
     rescue
       CaseClauseError -> nil
     end
-  end
+end
 
 
-  ##7 marzo 19
-    #Crear funcion que reciba la cadena y partirla reconociendo los tokens y devolverá una lista de tokens [{a, b}, {c, d}]
+##7 marzo 19
+  #Crear funcion que reciba la cadena y partirla reconociendo los tokens y devolverá una lista de tokens [{a, b}, {c, d}]
 
-    ##implementar algoritmo, tomando en cuenta las tuplas simples o dobles,
-  ##crear funcion que recibe un "programa" (la cadena words)
+  ##implementar algoritmo, tomando en cuenta las tuplas simples o dobles,
+##crear funcion que recibe un "programa" (la cadena words)
 
-  #ejecutar con "iex -S mix" para poder ejecutar la funcion
+#ejecutar con "iex -S mix" para poder ejecutar la funcion
 
-  #HACERLO RECURSIVO y romperlo mientras program no esté vacía
-  def lex_raw_tokens(program) when program != "" do #compara la cadena para ver que caso cumple
+#HACERLO RECURSIVO y romperlo mientras program no esté vacía
+def lex_raw_tokens(program) when program != "" do #compara la cadena para ver que caso cumple
 
 
-    #lo que devuela el case, haz pattern matching contra esto para recuperarlo...
-    try do
+  #lo que devuela el case, haz pattern matching contra esto para recuperarlo...
+  try do
     {token, cadena_restante} =
     case program do #case de la cadena
 
@@ -70,19 +70,21 @@ defmodule Lexer do #separando el lexer del modulo principal, mueve funcion (refa
         cadena_restante -> get_constant(cadena_restante) #crea funcion y pasale la cadena que no cumplió con los casos anteriores
         end
 
-    #devuelve recursirvamente la cadena_restante para seguir revisando
-    #tokens_restantes es la cadena restante con los caracteres sin separar hasta que se vacíe
-    tokens_restantes = lex_raw_tokens(cadena_restante) #añadir este token al final de la lista
-    [token | tokens_restantes] ##agrega los tokens restantes AL FINAL DE la lista de token
-  rescue
-    MatchError -> IO.inspect(program, label: "Error de sintaxis")
-  end
-  end
+  #devuelve recursirvamente la cadena_restante para seguir revisando
+  #tokens_restantes es la cadena restante con los caracteres sin separar hasta que se vacíe
+  tokens_restantes = lex_raw_tokens(cadena_restante) #añadir este token al final de la lista
+  [token | tokens_restantes] ##agrega los tokens restantes AL FINAL DE la lista de token
+rescue
+  MatchError -> IO.inspect(program, label: "Error de sintaxis")
+end
 
-  def lex_raw_tokens(_program) do #debe haber otra funcion llamada igual para recibir la cadena vacia (en realidad cualquiera)
-    [] ##para devolver una lista vacía
-  end
+end
 
-  #salida final del "exer" iex(3)> Proyecto_compilador.lex_raw_tokens("main(){}")
-  #[:main_keyword, :open_paren, :close_paren, :open_brace, :close_brace]
+def lex_raw_tokens(_program) do #debe haber otra funcion llamada igual para recibir la cadena vacia (en realidad cualquiera)
+
+  [] ##para devolver una lista vacía
+end
+
+#salida final del "exer" iex(3)> Proyecto_compilador.lex_raw_tokens("main(){}")
+#[:main_keyword, :open_paren, :close_paren, :open_brace, :close_brace]
 end
--- ProjectCompiler-twscc/lib/Saneador.ex
+++ ProjectCompiler-twscc/lib/Saneador.ex
@@ -8,26 +8,14 @@ defmodule Saneador do
     ##adición de pattern matching para encontrar la función main antes de comenzar la limpieza
     is_main = (file_content =~ "main")
     if is_main do
-      ##COMENTARIO BUENO: Sirve para limpiar la cadena de entrada y remover saltos de línea.
-      ##IO.puts("\nSaltos de línea (antes y despues del codigo) removidos (SANEADOR):");
       trimmed_content =  String.trim(file_content) ##borrara los saltos de linea antes y despues
-      ##IO.inspect(trimmed_content);
-      ##leer los tokens. Los numeros hacerles cast de String "2" a int 2
-      ##Keyword, INT, simbolo_Especial, etc... (etiquetas)
-      ##Implementar un autómata finito (AFD) usando expresiones regulares. Elixir ya tiene expresiones regulares
-      ##\s+ --> haz match con un espacio y saltos de linea desde 1 hasta los que encuentre
-      ##definicion de una expresion regular ~r/"expresion regular"/
-
-      ##IO.puts("\nSaltos de línea dentro del ćodigo removidos:");
       Regex.split(~r/\s+/, trimmed_content);
-      ##IO.inspect(words);
     else
-      IO.puts("Código inválido");
+      IO.puts("Código inválido: Falta la funcion main.");
+      nil
     end
 
 
   end
-
-
 
 end
