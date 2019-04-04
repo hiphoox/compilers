a0a9e598c5de594188df06381c6a74d7b4764667
CharlieML <charls_brian@hotmail.es>
2019-04-04 07:23:03 -0600

    Modulo Generador_codigo: postorden({atomo, value, izquierda ,derecha }, code): Se agrega atomo al parametro codigo_gen()/3 y postorden()/4

--- ProjectCompiler-twscc/lib/Generador_codigo.ex
+++ ProjectCompiler-twscc/lib/Generador_codigo.ex
@@ -10,12 +10,14 @@ defmodule Generador_codigo do
   #sin hijos el nodo
   defp postorder({}, code), do: code;
   #nodo con hijos
-  defp postorder({_atomo, value, izquierda ,derecha }, code) do
-    code = postorder(izquierda, code)
-    code = postorder(derecha, code) #devuelve un ok y continua
-    #IO.puts(value)
-    #IO.inspect(code)
-    codigo_gen(value, code);
+  defp postorden({atomo, value, izquierda ,derecha }, code) do
+    #recursividad, revisa {:node, data, {izquierda}, {}}
+    code = postorden(izquierda, code)
+    #recursividad, revisa {:node, data, {}, {derecha}}
+    code = postorden(derecha, code)
+    #si ya no encuentra más hijos, extrae el valor, genera el código y concatenalo con "code"
+    #IO.inspect(value, label: "Dato obtenido")
+    codigo_gen(atomo, value, code);
   end
 
   def codigo_gen("program", codigo) do
