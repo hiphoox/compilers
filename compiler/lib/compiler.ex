1cd40a82f28c8405ecb7274ab6970fe4c0adbf63
Mauricio <mauricioesquivel660@gmail.com>
2019-04-03 23:54:54 -0600

    Añade inicio de: sanitizer,linker y generador de codigo, se implmenta en main y se corrige variable de ats por ast

--- compiler/lib/compiler.ex
+++ compiler/lib/compiler.ex
@@ -3,7 +3,8 @@ defmodule Xforce do
 	def  main do
 			nom_file="test/example_S_1.c"
 			lista_toks=Lexer.main(nom_file)
-			arbol_ats=Parser.parse_function(lista_toks)
+			arbol_ast=Parser.parse_function(lista_toks)
+			generatedCode=CodeGenerator.generate_code(arbol_ast);
 	end
 
 end
