8cce0ec88ef98a358be82e33a6ab18d2e0703c14
Gustavo Ali Gómez Trejo <aligusali@gmail.com>
2019-04-06 11:06:25 -0600

    Se agrego el AST como se vio en clase y se empezo a realizar la comprobacion de los tokens en el parser

---
+++ camm_comp/lib/AST.ex
@@ -0,0 +1,3 @@
+defmodule AST do
+  defstruct [:nombre_nodo, :valor, :nodo_izq, :nodo_der] #Se agrego la estructura del arbol AST como vimos en clase
+end
--- camm_comp/lib/parser.ex
+++ camm_comp/lib/parser.ex
@@ -1,23 +1,20 @@
 defmodule Parser do
-	@type exp ::  integer
-
-  @type keywords :: {:intkeyword, charlist}
-									|{:returnkeyword, charlist}
-									|{:mainkeyword, charlist}
-
-	@type caracter :: {:open_brance, char}
-									| {:close_brance, char}
-									| {:open_parenthesis, char}
-									| {:close_parenthesis, char}
-									| {:semicolon, char}
-
-
-  @type statement :: keywords , exp , caracter
-
-  @type functions :: keywords, keywords, caracter,
-										caracter,caracter ,statement, caracter
-
-  @type program :: function
-
-	
+#Validamos que los token que recibimos sean los correctos
+  def parseo ([token | resto]) do
+  	if token == :int_keyword do
+  		[token | resto] =  resto
+			if token == :main_keyword do
+				[token | resto] = resto
+				if token == :open_paren do
+					[token | resto] = resto
+					if token == :close_paren do
+						[token | resto] = resto
+						if token == :open_brace do
+							[token | resto] = resto
+						end
+					end
+				end
+			end
+  	end
+  end
 end
