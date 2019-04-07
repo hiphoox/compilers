2da5ec9ea3515f3a04204f3da339ab9b63ea9c5e
Alejandro Hernández Albino <alejanndro.hdz@gmail.com>
2019-04-07 13:54:10 -0500

    Se creó un módulo para el saneador, y se modificaron dos funciones del módulo principal

--- camm_comp/lib/camm_comp.ex
+++ camm_comp/lib/camm_comp.ex
@@ -2,13 +2,11 @@ defmodule CammComp do
   @moduledoc """
   Documentation for Compilador
   """
-  def main(_args) do
-    file_content = File.read!("examples/e1.c")  # entre las comillas va la ubicación del archivo a asignar
-    trimmed_content = String.trim(file_content)  # saneamiento de codigo para exceso de espacios, tabulaciones
-    words = Regex.split(~r/\s+/,trimmed_content) # virulilla ‘r’ para declarar una expresión regular y se encierra en diagonales, woords es nuestra lista
-    tokens  = Lexer.scan_words(words)
-    IO.inspect(tokens)
-  end
+  def main(args) do
+  args
+  |> parse_args
+  |> process_args
+end
 
   def parse_args(args) do
     OptionParser.parse(args, switches: [help: :boolean])
@@ -33,8 +31,7 @@ defmodule CammComp do
    |> IO.inspect(label: "\nLexer ouput")
    |> Parser.parse_program()
    |> IO.inspect(label: "\nParser ouput")
-   |> CodeGenerator.generate_code()
-   |> Linker.generate_binary(assembly_path)
+
  end
 
  defp print_help_message do
