af34e16e4e2e51a16a63df0a284b3c5825d3c2f1
Yael3003 <keviniislas@gmail.com>
2019-04-07 13:16:49 -0500

    Se agrega estructura del arbol AST

---
+++ alfacompiler/compilers/lib/Ast.ex
@@ -0,0 +1,6 @@
+##Este es el AST que nos recomendo el prof
+##Se deben hacer modificaciones en el parse para poder usarlo
+
+defmodule AST do
+  defstruct [:node_name, :value, :left_node, :right_node]
+end
