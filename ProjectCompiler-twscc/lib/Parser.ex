cc2fc4b71ce8fcbb0b8ba59ff502e3bc08fbb100
CharlieML <charls_brian@hotmail.es>
2019-04-09 11:18:19 -0500

    Modulo Parser: parse_statement(t1): Se ha redefinido la funcion-> Sentencia case, pattern mattching

--- ProjectCompiler-twscc/lib/Parser.ex
+++ ProjectCompiler-twscc/lib/Parser.ex
@@ -49,11 +49,15 @@ defmodule Parser do
   end
 
   def parse_statement(tl) do
-    #devuelve los tokens restantes y el atomo extraido para el nombre del nodo
-    [remain_tl, elem] = tl |> parse_ret_value(:return_Keyword)
-    [tl, exp_node] = parse_exp(remain_tl);
-    remain_tl = tl |> parse(:semicolon)
-    [remain_tl, {elem, "return" , exp_node, {}}];
+    {atom, _value, tokens} = parse(tokens, :return_Keyword)
+
+    [tokens, exp_node] = parse_expression(tokens)
+
+    {_atom, _value, tokens} = parse(tokens, :semicolon)
+    case tokens do
+      {:error, _} -> [tokens, ""]
+      _ -> [tokens, {atom, "return", exp_node, {}}]
+    end
   end
 
   def parse_exp(tl) do
