a1605ddbba644b924af9f25fb24df9cd9eb40d3e
CharlieML <charls_brian@hotmail.es>
2019-04-09 11:36:22 -0500

    Modulo Linker: Se ha agragado la funcion binary_output(asm, flag, path)

--- ProjectCompiler-twscc/lib/Generador_codigo.ex
+++ ProjectCompiler-twscc/lib/Generador_codigo.ex
@@ -11,39 +11,37 @@ defmodule Generador_codigo do
   defp postorden({}, code), do: code;
   #nodo con hijos
   defp postorden({atomo, value, izquierda ,derecha }, code) do
-    #recursividad, revisa {:node, data, {izquierda}, {}}
     code = postorden(izquierda, code)
-    #recursividad, revisa {:node, data, {}, {derecha}}
     code = postorden(derecha, code)
     #si ya no encuentra más hijos, extrae el valor, genera el código y concatenalo con "code"
-    #IO.inspect(value, label: "Dato obtenido")
+
     codigo_gen(atomo, value, code);
   end
 
 #funciones "sobreescritas"
   def codigo_gen(:program, _, codigo) do
     """
-    main
     .p2align        4, 0x90
     """ <> codigo #concatena esto antes del codigo
   end
 
   def codigo_gen(:function, _, codigo) do
     """
-    .section	__TEXT,__text,regular,pure_instructions
-  	.intel_syntax noprefix
-  	.globl	_main                   ## -- Begin function
+      .globl  _main         ## -- Begin function main
+  _main:                    ## @main
   """ <> codigo #concatena esto antes del codigo
   end
 
   def codigo_gen(:constant, value, _) do
-    "#{value}" #guarda el valor de la constate
+    "$#{value}"
   end
+
   ##pega el valor de la constante y añade una instruccion return
   def codigo_gen(:return_Keyword, _, codigo) do
     """
-        mov    eax, #{codigo}
-        ret                            ## -- End function
+        movl    #{codigo}, %eax
+        ret
     """
   end
+
 end
--- ProjectCompiler-twscc/lib/Linker.ex
+++ ProjectCompiler-twscc/lib/Linker.ex
@@ -9,4 +9,13 @@ defmodule Linker do
   IO.puts("Para ejecutarlo utiliza el siguiente comando: ./your_program; echo $?")
   :ok
   end
+  def binary_output(asm, flag, path) do
+    case asm do
+      #la variable ASM traerá el error acarreado o bien, el string del ensamblador generado
+      #imprime en pantalla el motivo del error
+      {:error, _} -> IO.inspect(elem(asm, 1), label: "ERROR ")
+      _ -> generate_exe_or_asm(asm, flag, path)
+    end
+
+  end
 end
