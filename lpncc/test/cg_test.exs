d4da9e15a02ad545d46876385ebaa39173a593b1
galigaribaldi <galigaribaldi@live.com>
2019-04-08 23:32:46 -0500

    Se agregan las pruebas del parser

---
+++ lpncc/test/cg_test.exs
@@ -0,0 +1,41 @@
+defmodule Generador_CodigoTest do
+    use ExUnit.Case
+    doctest CodeGenerator
+    ### Documentacion}
+    setup_all do
+      {:ok,
+       tokens: [
+         :int_keyword,
+         :main_keyword,
+         :open_paren,
+         :close_paren,
+         :open_brace,
+         :return_keyword,
+         {:constant, 2},
+         :semicolon,
+         :close_brace
+       ],
+       tokens2: [
+         :int_keyword,
+         :main_keyword,
+         :open_paren,
+         :close_paren,
+         :open_brace,
+         :return_keyword,
+         {:constant, 0},
+         :semicolon,
+         :close_brace
+       ]}
+    end
+  
+    IO.puts("Pruebas para el Code Generator (Pruebas invalidas)")
+    IO.puts("7 Pruebas invalidas ")
+  
+    test "1.- Prueba", state do
+      assert "int main() return 2;}"
+             |> Sanitizer.sanitize_source()
+             |> Lexer.scan_words()
+             |> Parser.parseo() ==
+               {:error, "Falta una llave"}
+    end
+end
