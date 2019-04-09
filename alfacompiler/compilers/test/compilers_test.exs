3c8f6cff7b3b5edec80ff1e4e6b0d940d79920e8
Yael3003 <keviniislas@gmail.com>
2019-04-09 01:48:58 -0500

    Se agregan pruebas de Nora al codigo compilers_test

--- alfacompiler/compilers/test/compilers_test.exs
+++ alfacompiler/compilers/test/compilers_test.exs
@@ -45,4 +45,63 @@ defmodule CompilersTest do
     IO.puts("Spaces.c OK!!!!\n")
   end
 
+  test "Nora_multidigit" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/valid/multi_digit.c")]) == :ok
+    IO.puts("multi_digit.c OK!!!!\n")
+  end
+
+  test "Nora_newlines" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/valid/newlines.c")]) == :ok
+    IO.puts("newlines.c OK!!!!\n")
+  end
+
+  test "Nora_no_newlines" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/valid/no_newlines.c")]) == :ok
+    IO.puts("no_newlines.c OK!!!!\n")
+  end
+
+  test "Nora_newlines" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/valid/newlines.c")]) == :ok
+    IO.puts("newlines.c OK!!!!\n")
+  end
+
+  test "Nora_return_2" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/valid/return_2.c")]) == :ok
+    IO.puts("return_2.c OK!!!!\n")
+  end
+
+  
+
+
+  test "Nora_missing_paren" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/invalid/missing_paren.c")]) == :ok
+    IO.puts("missing_paren.c OK!!!!\n")
+  end
+
+  test "Nora_missing_retval" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/invalid/missing_retval.c")]) == :ok
+    IO.puts("missing_retval.c OK!!!!\n")
+  end
+
+  test "Nora_no_brace" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/invalid/no_brace.c")]) == :ok
+    IO.puts("no_brace.c OK!!!!\n")
+  end
+
+  test "Nora_no_semicolon" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/invalid/no_semicolon.c")]) == :ok
+    IO.puts("no_semicolon.c OK!!!!\n")
+  end
+
+  test "Nora_no_space" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/invalid/no_space.c")]) == :ok
+    IO.puts("no_space.c OK!!!!\n")
+  end
+
+  test "Nora_wrong_case" do
+    assert Compilers.call_nora_test([File.read!("test/Nora/invalid/wrong_case.c")]) == :ok
+    IO.puts("wrong_case.c OK!!!!\n")
+  end
+
+
 end
