f70172a3f1aaf2fe74c18241e1a4a39390ed9e52
Yael3003 <keviniislas@gmail.com>
2019-04-09 00:14:49 -0500

    Se agrega funcion para llamar y ejecutar las pruebas de Nora

--- alfacompiler/compilers/lib/compilers.ex
+++ alfacompiler/compilers/lib/compilers.ex
@@ -49,4 +49,16 @@ end
     |> Linker.generate_binary(assembly_path)
   end
 
+  def call_nora_test(string) do
+    IO.puts("El programa a compilar es")
+      IO.puts(string)
+      tokens_separados =  Lexer.scan_words(string); #lista de tokens
+      ast = Parser.parse_function(tokens_separados); #genera AST
+      #Generador_codigo.assembly(ast,file_address)|>Linker.genera_archivo_binario();
+      IO.inspect(tokens_separados);
+      IO.inspect(ast);
+      #Generador_codigo.assembly(ast,file_address)|>Linker.genera_archivo_binario();
+      :ok
+    end
+
 end
--- alfacompiler/compilers/test/compilers_test.exs
+++ alfacompiler/compilers/test/compilers_test.exs
@@ -2,7 +2,47 @@ defmodule CompilersTest do
   use ExUnit.Case
   doctest Compilers
 
-  test "greets the world" do
-    assert Compilers.hello() == :world
+  setup_all do
+    {:ok,
+     tokens: [
+       :int_keyword,
+       :main_keyword,
+       :a_parentesis,
+       :c_parentesis,
+       :a_llave,
+       :return_keyword,
+       {:constant, 2},
+       :semicolon,
+       :c_llave
+     ]}
   end
+
+  test "elements separated just by spaces", state do
+    assert Lexer.scan_words(["int", "main(){return", "2;}"]) == state[:tokens]
+  end
+
+  test "function name separated of function body", state do
+    assert Lexer.scan_words(["int", "main()", "{return", "2;}"]) == state[:tokens]
+  end
+
+  test "everything is separated", state do
+    assert Lexer.scan_words(["int", "main", "(", ")", "{", "return", "2", ";", "}"]) ==
+             state[:tokens]
+  end
+
+  test "main and body separated", state do 
+     assert Lexer.scan_words(["int", "main", "(", ")", "{return2;}"]) == state[:tokens]
+  end
+
+
+  test "todo junto", state do
+    assert Lexer.scan_words(["intmain(){return2;}"]) ==
+             state[:tokens]
+  end
+  
+  test "Nora_spaces" do
+    assert Compilers.call_nora_test(File.read!("test/Nora/valid/spaces.c")) == :ok
+    IO.puts("Spaces.c OK!!!!\n")
+  end
+
 end
