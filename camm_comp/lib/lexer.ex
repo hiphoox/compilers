9ce823778c594c0ad00a566b7277d677ea7b061e
martino2197 <martino2197@gmail.com>
2019-04-21 15:52:43 -0500

    Se arregla dentro del lexer la parte de codigo que muestra el error dentro de la lista de tokens

--- camm_comp/lib/lexer.ex
+++ camm_comp/lib/lexer.ex
@@ -3,50 +3,44 @@ defmodule Lexer do
     Enum.flat_map(words, &lex_raw_tokens/1)
   end
 
-  def get_constant(program) do
-		valor=Regex.run(~r/^\d+/, program)
-		if valor != :nil do
-			case valor do
-				[value] ->
-				{{:constant, String.to_integer(value)}, String.trim_leading(program, value)}
-			end
-		else
-			{"ERROR",program}
-		end
-	end
+  def get_constant(program, linea) do
+    valor = Regex.run(~r/^\d+/, program)
 
+    if valor != nil do
+      case valor do
+        [value] ->
+          {{{:constant, String.to_integer(value)}, linea}, String.trim_leading(program, value)}
+      end
+    else
+      {["ERROR", program, linea], ""}
+    end
+  end
 
+  def lex_raw_tokens({program, linea}) when program != "" do
+    linea_keyword = linea
 
-  def lex_raw_tokens(program) when program != "" do
     {token, rest} =
       case program do
         "int" <> rest -> {:intkeyword, rest}
-
         "return" <> rest -> {:returnkeyword, rest}
-
         "main" <> rest -> {:mainkeyword, rest}
-
         "{" <> rest -> {:open_brace, rest}
-
         "}" <> rest -> {:close_brace, rest}
-
         "(" <> rest -> {:open_parenthesis, rest}
-
         ")" <> rest -> {:close_parenthesis, rest}
-
         ";" <> rest -> {:semicolon, rest}
-
-        rest -> get_constant(rest)
+        # "n when is_bitstring(n)" <> rest -> {:pepe, rest}
+        rest -> get_constant(rest, linea)
       end
-	if rest != "" do
-		auxiliar_token={rest}
-		remaining_tokens=lex_raw_tokens(auxiliar_token)
-		toke=[token | remaining_tokens]
-	else
-		remaining_tokens=lex_raw_tokens(rest)
-		toke=[token | remaining_tokens]
-	end
 
+    if rest != "" do
+      auxiliar_token = {rest, linea}
+      remaining_tokens = lex_raw_tokens(auxiliar_token)
+      toke = [token | remaining_tokens]
+    else
+      remaining_tokens = lex_raw_tokens(rest)
+      toke = [token | remaining_tokens]
+    end
   end
 
   def lex_raw_tokens(_program) do
