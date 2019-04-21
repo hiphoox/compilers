3b8118ac7c087ba9f1be70884e5fb88aab0ccca9
martino2197 <martino2197@gmail.com>
2019-04-21 15:51:34 -0500

    Se agrega al sanitizer la parte de codigo encargada de enumerar los elementos de la lista de tokens

--- camm_comp/lib/sanitizer.ex
+++ camm_comp/lib/sanitizer.ex
@@ -1,6 +1,22 @@
 defmodule Sanitizer do
   def sanitize_source(file_content) do
     trimmed_content = String.trim(file_content)
-    Regex.split(~r/\s+/, trimmed_content)
+    words = Regex.split(~r/\s+/, trimmed_content)
+    contador = 0
+    hacer_lista(words, contador)
+  end
+
+  def hacer_lista(program, linea) when program != [] do
+    codigo_linea = hd(program)
+    otras_lineas = tl(program)
+    lista_token_sin_num = Regex.split(~r/\s+/, codigo_linea, trim: true)
+    lista_token_con_num = Enum.flat_map(lista_token_sin_num, fn x -> [{x, linea}] end)
+    linea = linea + 1
+    otrostokens = hacer_lista(otras_lineas, linea)
+    lista_token_con_num ++ otrostokens
+  end
+
+  def hacer_lista(program, linea) do
+    []
   end
 end
