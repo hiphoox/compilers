45953b150ac7a0a8a84f165144b2e7ce4d5539a2
CharlieML <charls_brian@hotmail.es>
2019-04-30 01:34:26 -0500

    Modulo Orquestador: Funcion manager(file,path,opt): Se ha definido la sentencia else de with

--- ProjectCompiler-twscc/lib/Orquestador.ex
+++ ProjectCompiler-twscc/lib/Orquestador.ex
@@ -1,19 +1,18 @@
 defmodule Orquestador do
-
   def manager(file, path, opt) do
-    #Utilizando "with" se procesa el archivo. Si hay error se detiene.
-    with  {:ok, tok} <- Lexer.scan_word(file, opt),
-          {:ok, ast} <- Parser.parse_tokens(tok, opt),
-          {:ok, asm} <- Generador_codigo.assembly(ast, opt, path),
-          {:ok, _} <- Linker.binary_output(asm, opt, path)
-          do
-          IO.puts("Finalizó la compilación correctamente.")
+  #Utilizando "with" se procesa el archivo. Si hay error se detiene.
+  with  {:ok, tok} <- Lexer.scan_word(file, opt),
+        {:ok, ast} <- Parser.parse_tokens(tok, opt),
+        {:ok, asm} <- Generador_codigo.assembly(ast, opt, path),
+        {:ok, _} <- Linker.binary_output(asm, opt, path)
+        do
+        IO.puts("Finalizó la compilación correctamente.")
   else
-        {:error, error} -> IO.puts(error) #mostrar motivo del error
-        {:only_tokens, _} -> IO.puts("TOKENS")
-        {:only_ast, _} -> IO.puts("AST")
-        {:only_asm,_} ->IO.puts("ASM")
-        #err -> err
+  #Se muestra el motivo del error o la salida de la opción seleccionada al compilar
+        {:error, error} -> IO.puts(error)
+        {:only_tokens, _} -> IO.puts("Lista de tokens.")
+        {:only_ast, _} -> IO.puts("Árbol Sintáctico Abstracto.")
+        {:only_asm, path_asm} ->IO.puts(path_asm)
       end
     end
 end
