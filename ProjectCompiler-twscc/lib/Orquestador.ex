01a50a14776aa93549fc6d1392a8d6596b8d0709
CharlieML <charls_brian@hotmail.es>
2019-04-30 01:33:27 -0500

    Modulo Orquestador: Funcion manager(file,path,opt): Se ha agregado la sentencia with

--- ProjectCompiler-twscc/lib/Orquestador.ex
+++ ProjectCompiler-twscc/lib/Orquestador.ex
@@ -1,20 +1,13 @@
 defmodule Orquestador do
 
   def manager(file, path, opt) do
-    #lectura y proceso del archivo, la tupla de error se propaga entre funciones
-  #archivo = File.read!(path)
-#    |> Lexer.scan_word(opt)
-#    |> Parser.parse_tokens(opt)
-  #  |> Generador_codigo.assembly(opt)
-  #  |> Linker.binary_output(opt, path); #necesitas la ruta para extraer el nombre del archivo
-  #  IO.puts("El compilador finalizó sin ningún error.")
-
-  with  {:ok, tok} <- Lexer.scan_word(file, opt),
-        {:ok, ast} <- Parser.parse_tokens(tok, opt),
-        {:ok, asm} <- Generador_codigo.assembly(ast, opt, path),
-        {:ok, exe} <- Linker.binary_output(asm, opt, path)
-        do
-        IO.puts("Finalizó el programa")
+    #Utilizando "with" se procesa el archivo. Si hay error se detiene.
+    with  {:ok, tok} <- Lexer.scan_word(file, opt),
+          {:ok, ast} <- Parser.parse_tokens(tok, opt),
+          {:ok, asm} <- Generador_codigo.assembly(ast, opt, path),
+          {:ok, _} <- Linker.binary_output(asm, opt, path)
+          do
+          IO.puts("Finalizó la compilación correctamente.")
   else
         {:error, error} -> IO.puts(error) #mostrar motivo del error
         {:only_tokens, _} -> IO.puts("TOKENS")
