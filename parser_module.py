f0e5feac5b982895e918735f4d29fa8b262b5fa0
Tenkoni <caliminion@gmail.com>
2019-05-20 21:51:38 -0500

    Se añade versión final del parser para la segunda entrega

--- parser_module.py
+++ parser_module.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 from treelib import Node, Tree
-
+import random
 
 def parser_f(token_list):
     ast_list = []
@@ -8,44 +8,49 @@ def parser_f(token_list):
     ast = Tree()
     ast.create_node('Program', 'prog')  # to create the first node of the AST
     # añadir a AST (program  hijo -> function)
-    # if token_list:
-    #     raise SystemExit(
-    #         "Syntax error: There are more elements after the function end.")
+    if token_list:
+        raise SystemExit("Syntax error: There are more elements after the function end.")
 
+    #print(ast_list) #debug purposes
     # # to create the rest of the nodes of the tree from the list
-    # for leaf in reversed(ast_list):
-    #     if 'Constant' in leaf[0]:
-    #         ast.create_node(leaf[0], leaf[1], parent='expr', data=leaf[2][1])
-    #     if 'Expression' in leaf[0]:
-    #         ast.create_node(leaf[0], leaf[1], parent='func', data=leaf[2])
-    #     if 'Function' in leaf[0]:
-    #         ast.create_node(leaf[0], leaf[1], parent='prog', data=leaf[2])
-    # return ast
-    return ast_list
+    for index, leaf in enumerate(ast_list):
+        if 'negation' in leaf[0] or "bitwise_complement" in leaf[0] or "logical_negation" in leaf[0]:
+            ast.create_node(leaf[0], leaf[1], parent = ast_list[index-1][1], data = leaf[2])
+        if 'Constant' in leaf[0]:
+            ast.create_node(leaf[0], leaf[1], parent = ast_list[index-1][1], data=leaf[2][1])
+        if 'Expression' in leaf[0]:
+            ast.create_node(leaf[0], leaf[1], parent='func', data=leaf[2])
+        if 'Function' in leaf[0]:
+            ast.create_node(leaf[0], 'func', parent='prog', data=leaf[2])
+    return ast
 
 
 def function_parser(token_list, ast_list):
     if token_list.pop(0) != 'keyword_int':
         raise SystemExit("Syntax error, return type value missing.")
-    if token_list.pop(0) != 'keyword_main':
-        raise SystemExit("Syntax error, main function missing.")
+    temporal_token = token_list.pop(0)
+    ast_list.append(['Function:main', str(random.randint(1,10000)), temporal_token]) if temporal_token == 'keyword_main' else raiser( SystemExit("Syntax error, main function missing."))
     if token_list.pop(0) != 'parentheses_open':
         raise SystemExit("Syntax error, open parentheses missing.")
     if token_list.pop(0) != 'parentheses_close':
         raise SystemExit("Syntax error, close parentheses missing.")
-    statement_parser(token_list, ast_list) if token_list.pop(0) == 'bracket_open' else raiser(SystemExit("Syntax error, open bracket missing."))
-    temporal_token = token_list.pop(0)
-    ast_list.append(['Function:main', 'func', temporal_token]) if temporal_token == 'bracket_close' else raiser(
-        SystemExit("Syntax error, close bracket missing."))
+    if token_list.pop(0) != 'bracket_open':
+        raiser(SystemExit("Syntax error, open bracket missing."))
+    statement_parser(token_list, ast_list)
+    if token_list.pop(0) != 'bracket_close':
+        raiser(SystemExit("Syntax error, close bracket missing."))
     # añadir a AST (nombre -> function ; value -> main ; hijo-> statement), por el momento print será un placeholder
 
 
 def statement_parser(token_list, ast_list):
-    expression_parser(token_list, ast_list) if token_list.pop(0) == 'keyword_return' else raiser(SystemExit("Syntax error, return keyword missing."))
     temporal_token = token_list.pop(0)
-    print(temporal_token)
-    ast_list.append(['Expression:return', 'expr', temporal_token]) if temporal_token == 'semicolon' else raiser(
-        SystemExit("Syntax error, semicolon missing after constant to finish return statment."))
+    if temporal_token == 'keyword_return':
+        ast_list.append(['Expression:return', str(random.randint(1,10000)), temporal_token])
+        expression_parser(token_list, ast_list) 
+    else:
+        raiser(SystemExit("Syntax error, return keyword missing."))
+    if token_list.pop(0) != 'semicolon':
+        raiser(SystemExit("Syntax error, semicolon missing after constant to finish return statment."))
     # añadir a AST (nombre -> return, hijo -> expresion), por el momento print será un placeholder
 
 
@@ -58,7 +63,7 @@ def expression_parser(token_list, ast_list):
     term_parser(token_list, ast_list)
     t = token_list[0] if token_list else SystemExit("Parser ran out of tokens")
     while (t == "addition" or t == "negation"):
-        ast_list.append([t, t, t])
+        ast_list.append([t, str(random.randint(1,10000)), t])
         token_list.pop(0)
         term_parser(token_list, ast_list)
         t = token_list[0] if token_list else SystemExit("Parser ran out of tokens")
@@ -70,7 +75,7 @@ def term_parser(token_list, ast_list):
     factor_parser(token_list, ast_list)
     t = token_list[0] if token_list else SystemExit("Parser ran out of tokens")
     while (t == "multiplication" or t == "division"):
-        ast_list.append([t, t, t])
+        ast_list.append([t, str(random.randint(1,10000)), t])
         token_list.pop(0)
         factor_parser(token_list, ast_list)
         t = token_list[0] if token_list else SystemExit("Parser ran out of tokens")
@@ -89,12 +94,12 @@ def factor_parser(token_list, ast_list):
             return
     if (t == "negation" or t == 'bitwise_complement' or t == 'logical_negation'):
         token_list.pop(0)
-        ast_list.append([t, t, t])
+        ast_list.append([t, str(random.randint(1,10000)), t])
         factor_parser(token_list, ast_list)
         return
 
     if tuple == type(t):
-        ast_list.append(['Constant:'+str(t[1]), 'const', t])
+        ast_list.append(['Constant:'+str(t[1]), str(random.randint(1,10000)), t])
         token_list.pop(0)
         return
 
