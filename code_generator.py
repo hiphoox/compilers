b2c6a0554abad64eed06115143f4e1cdc94c85ac
Tenkoni <caliminion@gmail.com>
2019-05-21 00:28:20 -0500

    Se actualiza generador de código para funcionar con la semana 2

--- code_generator.py
+++ code_generator.py
@@ -6,12 +6,31 @@ def generator(ast):
 
 
 def assembler_generator(ast):
+    assembler_list = []
     assembler = ""
-    constant_leaves = ast.is_branch('expr')
-    for leaf in constant_leaves:  # this for is going to be useful when we get more than one constant in the next part of the project
-        if not ast.is_branch(leaf):
-            assembler = expression_code(constant_code(ast.get_node(leaf).data))
-    assembler = program_code() + function_code() + assembler
+    #constant_leaves = ast.is_branch('expr')
+    #for leaf in constant_leaves:  # this for is going to be useful when we get more than one constant in the next part of the project
+    #    if not ast.is_branch(leaf):
+    #        assembler = expression_code(constant_code(ast.get_node(leaf).data))
+    #
+    nodeid = 'func'
+    while(ast.is_branch(nodeid)):
+        nodeid = ast.is_branch(nodeid)[0]
+        assembler_list.append(ast.get_node(nodeid).tag)
+    assembler_list.pop(0)
+    for node in reversed(assembler_list):
+        if 'logical_negation' in node:
+            assembler += logic_neg()
+        if 'bitwise_complement' in node:
+            assembler += bitewise_code()
+        if 'negation' in node:
+            assembler += negation_code()
+        if 'Constant' in node:
+            assembler += expression_code(constant_code(node.split(":")[1]))
+    print(assembler_list)
+    assembler = program_code() + function_code() + assembler + return_code()
+    print(assembler)
+
     return assembler
 
 
@@ -30,10 +49,32 @@ _main:                    ## @main
 
 
 def expression_code(cte_snip):
-    code_snip = "movl    " + cte_snip + ", %eax" + "\n" + "    ret"
+    code_snip = "\n    movl    " + cte_snip + ", %eax"
     return code_snip
 
+def return_code():
+    code_snip = "\n" +"    ret"
+    return code_snip
 
 def constant_code(constant_value):
     code_snip = "$"+str(constant_value)
     return code_snip
+
+def bitewise_code():
+    code_snip = "\n"+"    not    %eax"
+    return code_snip
+
+def negation_code():
+    code_snip = """
+    neg    %eax
+        """
+    return code_snip
+
+def logic_neg():
+    code_snip ="""
+        cmpl    $0, %eax
+        movl    $0, %eax
+        sete    %al
+    """
+    return code_snip
+
