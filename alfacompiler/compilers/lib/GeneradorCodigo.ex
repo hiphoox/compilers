3c354dcf8861f4b39f8aa907ff6db227facf7891
PaolaElizabethSanchez <paolaescastillo@gmail.com>
2019-05-22 15:38:22 -0500

    Se sube la primer versión de la lectura de unarios

--- alfacompiler/compilers/lib/GeneradorCodigo.ex
+++ alfacompiler/compilers/lib/GeneradorCodigo.ex
@@ -1,21 +1,16 @@
 defmodule CodeGenerator do
   def generate_code(ast) do
-    code = post_order(ast)
-    IO.puts("\nCode Generator output:")
-    IO.puts(code)
-    code
+   post_order(ast)
+
   end
 
   def post_order(node) do
-    case node do     
-
-    
+    case node do
       nil ->
         nil
 
       ast_node ->
         code_snippet = post_order(ast_node.left_node)
-        # TODO: Falta terminar de implementar cuando el arbol tiene mas ramas
         post_order(ast_node.right_node)
         emit_code(ast_node.node_name, code_snippet, ast_node.value)
     end
@@ -23,28 +18,48 @@ defmodule CodeGenerator do
 
   def emit_code(:program, code_snippet, _) do
     """
-        .section       
-        .p2align        4, 0x90
     """ <>
       code_snippet
   end
 
   def emit_code(:function, code_snippet, :main) do
     """
-        .globl  _main         ## -- Begin function main
-    _main:                    ## @main
+	       .globl	main
+    main:
     """ <>
       code_snippet
   end
 
   def emit_code(:return, code_snippet, _) do
+    code_snippet<>
     """
-        movl    #{code_snippet}, %eax
         ret
+    """
+  end
+  def emit_code(:negation, code_snippet, _) do
+    code_snippet<>
+    """
+        neg	%eax
+    """
+  end
+  def emit_code(:complement, code_snippet, _) do
+    code_snippet<>
+    """
+        not	%eax
+    """
+  end
+  def emit_code(:logical, code_snippet, _) do
+    code_snippet<>
+    """
+    cmpl     $0, %eax
+    movl     $0, %eax
+    sete     %al
     """
   end
 
   def emit_code(:constant, _code_snippet, value) do
-    "$#{value}"
+    """
+        movl	$#{value}, %eax
+    """
   end
 end
