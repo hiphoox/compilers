3adae95e8208fa119a642ddf8c38e683d66f8750
Omar Orlando <orlando.ortega@gm3s.com.mx>
2019-06-07 08:40:55 -0500

    Modificación Parser para 3ra Entrega
     -Se modifica el parser para poder manejar lo solicitado en la 3er semana

--- lpncc/lib/parser.ex
+++ lpncc/lib/parser.ex
@@ -77,7 +77,7 @@ defmodule Parser do
 
   def parse_statement([{next_token,numline} | rest]) do
       if next_token == :return_keyword do
-        expression = parse_expression(rest)
+        expression = parse_expression(rest,1)
         case expression do
           {{:error, error_message,numline,next_token}, rest} ->
             {{:error, error_message,numline,next_token}, rest}
@@ -93,48 +93,108 @@ defmodule Parser do
       else
         {{:error, "Error #8",numline,next_token}, rest}
       end
-    end
+  end
 
-  def parse_expression([{next_token,numline} | rest]) do
-    case next_token do
-      {:constant, value} ->
-        {%AST{node_name: :constant, value: value}, rest}
-    :complement_keyword->
-      unary_op([{next_token,numline} | rest])
-    :negative_keyword->
-      unary_op([{next_token,numline} | rest])
-    :negative_logical ->
-      unary_op([{next_token,numline} | rest])
-    _->
-    {{:error, "Error en arbol 2",numline,:constant}, rest}
+  def parse_expression(lista_tkn, cExp) do
+    [{next_token,numline} | rest]=lista_tok
+
+      term = parse_term(lista_tok,1)
+      {expression_node,lista_rest}=term
+      [{next_tok,numline}|rest]=lista_rest
+      case next_tok do
+      :sum ->
+        sTree=%AST{node_name: :sum}
+        top = parse_expression(rest,1)
+        {node,lista_rest}=top
+        [{next_tok,numline}|restop]=lista_rest
+        {%{sTree | left_node: expression_node, right_node: node }, lista_rest}
+      :negative_keyword->
+        sTree=%AST{node_name: :rest}
+        top = parse_expression(rest,1)
+        {node,lista_rest}=top
+      [{next_tok,numline}|restop]=lista_rest
+      {%{sTree | left_node: expression_node, right_node: node }, lista_rest}
+      _->
+      term
     end
   end
 
-def next_term ([{next_token,numline}] | rest) do
-    case next_token do
-      
-    end
-end
-def parse_term ([{next_token,numline}]| rest)
+  def parse_term(ast_tree, cTerm) do
+    [{next_token,numline} | rest]=ast_tree
+          factor = parse_factor(ast_tree)
+          {expression_node,lista_rest}=factor
+          [{sig_tok,numline}|rest]=lista_rest
+          case sig_tok do
+          :multiplication ->
+              sTree=%AST{node_name: :multiplication}
+              top = parse_expression(rest,1)
+              {node,lista_rest}=top
+              [{sig_tok,numline}|restop]=lista_rest
+              {%{sTree | left_node: expression_node, right_node: node }, lista_rest}
+            :division->
+              sTree=%AST{node_name: :division}
+              top = parse_expression(rest,1)
+              {node,lista_rest}=top
+              [{sig_tok,numline}|restop]=lista_rest
+              {%{sTree | left_node: expression_node, right_node: node }, lista_rest}
+          _->
+            factor
+          end
+  end
 
-end
+  def parse_factor(arbol_AST) do
+      [{next_token,numline} | rest]=arbol_AST
+      case next_token do
+        :open_paren->
+          if next_token==:open_paren do
+            expression=parse_expression(rest,1)
+            case expression do
+              {{:error, error_message}, rest} ->
+                  {{:error, error_message}, rest}
 
-def parse_factor ([{next_token,numline}]| rest)
+              {expression_node,lista_rest} ->
+                    [{next_token,numline}|rest]=lista_rest
+                if next_token == :close_paren do
+                    {expression_node, rest}
+                  else
+                    express=parse_expression(rest,1)
+                    {node_expression,lista_rest_expression}=expression
+                    {node,lista_rest_node}=express
+                    [_|lista_sin_open_parent]=lista_rest_node
+                    {%{node_expression | left_node: node}, lista_sin_open_parent}
+                  end
+            end
+          else
+            {{:error, "Error: factor '(' ",numline,next_token}, rest}
+          end
+          :complement_keyword->
+        unary_op([{next_token,numline} | rest])
+          :negative_keyword->
+        unary_op([{next_token,numline} | rest])
+          :negative_logical ->
+        unary_op([{next_token,numline} | rest])
 
-do
+        {:constant, value} ->
+
+          {%AST{node_name: :constant, value: value}, rest}
+        _->
+        {{:error, "Error: factor (mas elementos)",numline,next_token}, rest}
+
+      end
+  end
 
   def unary_op([{next_token,numline} | rest]) do
     case next_token do
       :negative_keyword ->
-        parexpres=parse_expression(rest)
+        parexpres=parse_factor(rest)
         {nodo,rest_necesario}=parexpres
         {%AST{node_name: :unary_negative, left_node: nodo}, rest_necesario}
       :complement_keyword ->
-        parexpres=parse_expression(rest)
+        parexpres=parse_factor(rest)
         {nodo,rest_necesario}=parexpres
         {%AST{node_name: :unary_complement, left_node: nodo}, rest_necesario}
       :negative_logical ->
-        parexpres=parse_expression(rest)
+        parexpres=parse_factor(rest)
         {nodo,rest_necesario}=parexpres
         {%AST{node_name: :negative_logical, left_node: nodo}, rest_necesario}
       _ -> {{:error, "Error en arbol 1",numline,next_token}, rest}
