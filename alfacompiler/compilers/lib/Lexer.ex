1e9e213037fe02c639eae9650bef912093b76c92
PaolaElizabethSanchez <paolaescastillo@gmail.com>
2019-06-07 12:26:15 -0500

    Se añaden tockens corregidos

--- alfacompiler/compilers/lib/Lexer.ex
+++ alfacompiler/compilers/lib/Lexer.ex
@@ -20,39 +20,50 @@ defmodule Lexer do
 
     {token, rest} =
       case program do
-        "int" <> rest -> 
+        "int" <> rest ->
           {:int_keyword, rest}
-        
-        "return" <> rest -> 
+
+        "return" <> rest ->
           {:return_keyword, rest}
-        
-        "main" <> rest -> 
+
+        "main" <> rest ->
           {:main_keyword, rest}
-        
-        "{" <> rest -> 
+
+        "{" <> rest ->
           {:a_llave, rest}
-       
-        "}" <> rest -> 
+
+        "}" <> rest ->
           {:c_llave, rest}
-       
-        "(" <> rest -> 
+
+        "(" <> rest ->
           {:a_parentesis, rest}
-       
-        ")" <> rest -> 
+
+        ")" <> rest ->
           {:c_parentesis, rest}
 
-        "-" <> rest -> 
+        "-" <> rest ->
           {:negacion, rest}
-         
-        "~" <> rest -> 
+
+
+          " + "  <> rest ->
+            { : add , rest}
+
+          " * "  <> rest ->
+            { : mult , rest}
+
+          " / "  <> rest ->
+            { : div , rest}
+
+
+        "~" <> rest ->
           {:complemento, rest}
-         
-        "!" <> rest -> 
+
+        "!" <> rest ->
           {:logical_negation, rest}
-       
-        ";" <> rest -> 
+
+        ";" <> rest ->
           {:semicolon, rest}
-       
+
         rest -> get_constant(rest)
       end
 
