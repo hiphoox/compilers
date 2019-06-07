d92214700f915e77edb876f5f400cc5b25c99669
Erik Valdez Mondragon <perik_92@hotmail.com>
2019-06-07 11:10:20 -0500

    Se han borrado ejecutables y ensambladores generados

--- ProjectCompiler-twscc/test/add.s
+++
@@ -1,9 +0,0 @@
-.p2align        4, 0x90
-    .globl  main         ## -- Begin function main
-main:                    ## @main
-    mov     $1, %rax
-    push    %rax
-    mov     $2, %rax
-    pop     %rcx
-    add     %rcx, %rax
-    ret
--- ProjectCompiler-twscc/test/div.s
+++
@@ -1,9 +0,0 @@
-.p2align        4, 0x90
-    .globl  main         ## -- Begin function main
-main:                    ## @main
-    mov     $4, %rax
-    push    %rax
-    mov     $2, %rax
-    pop     %rcx
-    div     %rcx
-    ret
